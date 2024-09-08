bd82d01d0aca171c98bdbe3e8ae672fcb549f657
Trent <wantmantrent@ufl.edu>
2024-09-07 20:07:00 -0400

    Rewriting the game architecture

---
+++ cmake-build-debug/CMakeFiles/Mines.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Mines.dir/main.cpp.obj"
+  "CMakeFiles/Mines.dir/main.cpp.obj.d"
+  "CMakeFiles/Mines.dir/random.cpp.obj"
+  "CMakeFiles/Mines.dir/random.cpp.obj.d"
+  "CMakeFiles/Mines.dir/texture.cpp.obj"
+  "CMakeFiles/Mines.dir/texture.cpp.obj.d"
+  "Mines.exe"
+  "Mines.exe.manifest"
+  "Mines.pdb"
+  "libMines.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Mines.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
