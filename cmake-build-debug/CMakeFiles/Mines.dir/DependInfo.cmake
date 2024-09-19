96e0f110cb64c46597a3d618d5960f2b577303c4
wprods02 <wantmantrent@gmail.com>
2024-09-19 19:39:58 -0400

    Found better solution to easter egg handling. Still in the process.

--- cmake-build-debug/CMakeFiles/Mines.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/Mines.dir/DependInfo.cmake
@@ -8,19 +8,19 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "C:/Users/litte/CLionProjects/Mines/BetButton.cpp" "CMakeFiles/Mines.dir/BetButton.cpp.obj" "gcc" "CMakeFiles/Mines.dir/BetButton.cpp.obj.d"
-  "C:/Users/litte/CLionProjects/Mines/CashoutButton.cpp" "CMakeFiles/Mines.dir/CashoutButton.cpp.obj" "gcc" "CMakeFiles/Mines.dir/CashoutButton.cpp.obj.d"
-  "C:/Users/litte/CLionProjects/Mines/EasterEgg.cpp" "CMakeFiles/Mines.dir/EasterEgg.cpp.obj" "gcc" "CMakeFiles/Mines.dir/EasterEgg.cpp.obj.d"
-  "C:/Users/litte/CLionProjects/Mines/GameController.cpp" "CMakeFiles/Mines.dir/GameController.cpp.obj" "gcc" "CMakeFiles/Mines.dir/GameController.cpp.obj.d"
-  "C:/Users/litte/CLionProjects/Mines/MineOptions.cpp" "CMakeFiles/Mines.dir/MineOptions.cpp.obj" "gcc" "CMakeFiles/Mines.dir/MineOptions.cpp.obj.d"
-  "C:/Users/litte/CLionProjects/Mines/Texture.cpp" "CMakeFiles/Mines.dir/Texture.cpp.obj" "gcc" "CMakeFiles/Mines.dir/Texture.cpp.obj.d"
-  "C:/Users/litte/CLionProjects/Mines/Tile.cpp" "CMakeFiles/Mines.dir/Tile.cpp.obj" "gcc" "CMakeFiles/Mines.dir/Tile.cpp.obj.d"
-  "C:/Users/litte/CLionProjects/Mines/UI.cpp" "CMakeFiles/Mines.dir/UI.cpp.obj" "gcc" "CMakeFiles/Mines.dir/UI.cpp.obj.d"
-  "C:/Users/litte/CLionProjects/Mines/WagerOptions.cpp" "CMakeFiles/Mines.dir/WagerOptions.cpp.obj" "gcc" "CMakeFiles/Mines.dir/WagerOptions.cpp.obj.d"
-  "C:/Users/litte/CLionProjects/Mines/WallPaper.cpp" "CMakeFiles/Mines.dir/WallPaper.cpp.obj" "gcc" "CMakeFiles/Mines.dir/WallPaper.cpp.obj.d"
-  "C:/Users/litte/CLionProjects/Mines/Wallpapers.cpp" "CMakeFiles/Mines.dir/Wallpapers.cpp.obj" "gcc" "CMakeFiles/Mines.dir/Wallpapers.cpp.obj.d"
-  "C:/Users/litte/CLionProjects/Mines/main.cpp" "CMakeFiles/Mines.dir/main.cpp.obj" "gcc" "CMakeFiles/Mines.dir/main.cpp.obj.d"
-  "C:/Users/litte/CLionProjects/Mines/random.cpp" "CMakeFiles/Mines.dir/random.cpp.obj" "gcc" "CMakeFiles/Mines.dir/random.cpp.obj.d"
+  "C:/Users/Trent/Desktop/MinesGame/BetButton.cpp" "CMakeFiles/Mines.dir/BetButton.cpp.obj" "gcc" "CMakeFiles/Mines.dir/BetButton.cpp.obj.d"
+  "C:/Users/Trent/Desktop/MinesGame/CashoutButton.cpp" "CMakeFiles/Mines.dir/CashoutButton.cpp.obj" "gcc" "CMakeFiles/Mines.dir/CashoutButton.cpp.obj.d"
+  "C:/Users/Trent/Desktop/MinesGame/EasterEgg.cpp" "CMakeFiles/Mines.dir/EasterEgg.cpp.obj" "gcc" "CMakeFiles/Mines.dir/EasterEgg.cpp.obj.d"
+  "C:/Users/Trent/Desktop/MinesGame/GameController.cpp" "CMakeFiles/Mines.dir/GameController.cpp.obj" "gcc" "CMakeFiles/Mines.dir/GameController.cpp.obj.d"
+  "C:/Users/Trent/Desktop/MinesGame/MineOptions.cpp" "CMakeFiles/Mines.dir/MineOptions.cpp.obj" "gcc" "CMakeFiles/Mines.dir/MineOptions.cpp.obj.d"
+  "C:/Users/Trent/Desktop/MinesGame/Texture.cpp" "CMakeFiles/Mines.dir/Texture.cpp.obj" "gcc" "CMakeFiles/Mines.dir/Texture.cpp.obj.d"
+  "C:/Users/Trent/Desktop/MinesGame/Tile.cpp" "CMakeFiles/Mines.dir/Tile.cpp.obj" "gcc" "CMakeFiles/Mines.dir/Tile.cpp.obj.d"
+  "C:/Users/Trent/Desktop/MinesGame/UI.cpp" "CMakeFiles/Mines.dir/UI.cpp.obj" "gcc" "CMakeFiles/Mines.dir/UI.cpp.obj.d"
+  "C:/Users/Trent/Desktop/MinesGame/WagerOptions.cpp" "CMakeFiles/Mines.dir/WagerOptions.cpp.obj" "gcc" "CMakeFiles/Mines.dir/WagerOptions.cpp.obj.d"
+  "C:/Users/Trent/Desktop/MinesGame/WallPaper.cpp" "CMakeFiles/Mines.dir/WallPaper.cpp.obj" "gcc" "CMakeFiles/Mines.dir/WallPaper.cpp.obj.d"
+  "C:/Users/Trent/Desktop/MinesGame/Wallpapers.cpp" "CMakeFiles/Mines.dir/Wallpapers.cpp.obj" "gcc" "CMakeFiles/Mines.dir/Wallpapers.cpp.obj.d"
+  "C:/Users/Trent/Desktop/MinesGame/main.cpp" "CMakeFiles/Mines.dir/main.cpp.obj" "gcc" "CMakeFiles/Mines.dir/main.cpp.obj.d"
+  "C:/Users/Trent/Desktop/MinesGame/random.cpp" "CMakeFiles/Mines.dir/random.cpp.obj" "gcc" "CMakeFiles/Mines.dir/random.cpp.obj.d"
   )
 
 # Targets to which this target links which contain Fortran sources.
